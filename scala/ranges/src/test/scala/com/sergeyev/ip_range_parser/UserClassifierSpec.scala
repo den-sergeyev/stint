bc42cda66aef7171eb325a9cfd15168583eadb14
Dzianis Siarheyeu <dzianis_siarheyeu@epam.com>
2016-02-25 00:46:32 +0300

    Add test stub

---
+++ scala/ranges/src/test/scala/com/sergeyev/ip_range_parser/UserClassifierSpec.scala
@@ -0,0 +1,30 @@
+package com.sergeyev.ip_range_parser
+
+import org.specs2.mutable._
+
+class UserClassifclassierSpec extends Specification {
+  "UserClassifier#parseNetworksMapping" should {
+    "compose map from subnetwork definition file" in {
+       ok
+     }
+  }
+
+  "UserClassifier#classify" should {
+    "read user stats file and categorize each stat to subnet" in {
+       ok
+     }
+  }
+
+  "UserClassifier#convertIpToLong" should {
+    "convert ip addres to long value" in {
+       ok
+     }
+  }
+
+  "UserClassifier#write" should {
+    "write txt to file at path" in {
+      ok
+     }
+  }
+}
+
