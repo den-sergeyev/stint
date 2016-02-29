d45956ef819b84a451a18badaae0ef5a942c60f3
Dzianis Siarheyeu <dzianis_siarheyeu@epam.com>
2016-02-29 21:28:29 +0300

    Implement K-ordered statistics

---
+++ ruby/data_structures/lib/algo/k_ordered_statistics.rb
@@ -0,0 +1,18 @@
+module Algo::KOrderedStatistics
+
+  def find_statistics(array, order)
+    left = 0
+    right = array.size
+
+
+
+
+
+  end
+
+  private
+  def pivot_for(array)
+    array.first
+  end
+end
+
